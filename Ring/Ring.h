433ab103b837cf0e60a620ab05fc9039f2595245
Cesar Tardaguila <cesar@yyresearch.com>
2016-04-05 09:36:17 +0800

    Build as a framework, and use the framework in the sample app

---
+++ Ring/Ring.h
@@ -0,0 +1,19 @@
+//
+//  Ring.h
+//  Ring
+//
+//  Created by Cesar Tardaguila on 4/5/16.
+//  Copyright © 2016 Cesar Tardaguila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ring.
+FOUNDATION_EXPORT double RingVersionNumber;
+
+//! Project version string for Ring.
+FOUNDATION_EXPORT const unsigned char RingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ring/PublicHeader.h>
+
+
